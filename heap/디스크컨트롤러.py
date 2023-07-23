ae7933ab5aab46f0d07daad732b2d37ae13a6e39
seungalee <renazzang01@naver.com>
2023-07-24 06:05:55 +0900

    Update 디스크컨트롤러.py
    테스트케이스 하나는 맞았으나 전체로 보니 틀림

--- heap/디스크컨트롤러.py
+++ heap/디스크컨트롤러.py
@@ -3,13 +3,15 @@ from heapq import heapify, heappop, heappush
 
 def solution(jobs):
     heapify(jobs)
+    lenofJobs = len(jobs)
     time = 0
-    readyDisk = []
+    
     nowDisk = heappop(jobs)
     time += nowDisk[1]
     totaltime = nowDisk[0]+nowDisk[1]
     
     while len(jobs)>0:
+        readyDisk = []
         while True:
             nowDisk = heappop(jobs)
             if nowDisk[0] <= time:
@@ -19,11 +21,18 @@ def solution(jobs):
                 break
             if len(jobs)==0:
                 break
-        nextjobIndex = readyDisk.index(min(readyDisk[1]))
-        time = readyDisk[nextjobIndex][1]
+        #여기서 에러발생: 2차원 배열의 특정 행에서 최소값 어떻게 찾지?
+        readyDiskWorkTime = []
+        for i in readyDisk:
+            readyDiskWorkTime.append(i[1])
+        nextjobIndex = readyDiskWorkTime.index(min(readyDiskWorkTime))
+        time += readyDisk[nextjobIndex][1]
         totaltime += (time-readyDisk[nextjobIndex][0])
-        answer = totaltime/(len(jobs))
-        return answer
+        del readyDisk[nextjobIndex]
+        for i in readyDisk:
+            heappush(jobs, i)
+    answer = totaltime/lenofJobs
+    return answer
 
 jobs = [[0, 3], [1, 9], [2, 6]]
 solution(jobs)

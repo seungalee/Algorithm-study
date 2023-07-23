2f09c175107814c394f6499255d2d88dba343b45
seungalee <renazzang01@naver.com>
2023-07-24 03:09:10 +0900

    heap:디스크 컨트롤러(실패)

---
+++ heap/디스크컨트롤러.py
@@ -0,0 +1,23 @@
+from heapq import heapify, heappop, heappush
+jobs = [[0, 3], [1, 9], [2, 6]]
+
+def solution(jobs):
+    heapify(jobs)
+    time = 0
+    readyDisk = []
+    nowDisk = heappop(jobs)
+    time += nowDisk[1]
+    totaltime = nowDisk[0]+nowDisk[1]
+    
+    while True:
+        nowDisk = heappop(jobs)
+        if nowDisk[1] <= time:
+            readyDisk.append(nowDisk)
+        else:
+            heappush(jobs, nowDisk)
+            break
+    nextjobIndex = readyDisk.index(min(readyDisk[1]))
+    time = readyDisk[nextjobIndex][1]
+    totaltime += (time-readyDisk[nextjobIndex][0])
+    answer = totaltime/(len(jobs))
+    return answer

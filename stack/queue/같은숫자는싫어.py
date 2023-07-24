eb186ea2eccc142a3644b198e896efbdb66f3521
seungalee <renazzang01@naver.com>
2023-07-25 06:21:18 +0900

    Update 같은숫자는싫어.py
    단순화함

--- stack/queue/같은숫자는싫어.py
+++ stack/queue/같은숫자는싫어.py
@@ -2,12 +2,9 @@ def solution(arr):
     answer = []
     for i in arr:
         if answer:
-            curr = answer.pop()
+            curr = answer[-1]
             if curr!=i:
-                answer.append(curr)
                 answer.append(i)
-            else:
-                answer.append(curr)
         else: 
             answer.append(i)
     return answer

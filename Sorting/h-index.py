bfc7d751a1ae3f85327f1e9f9cc50c3090b68162
seungalee <renazzang01@naver.com>
2023-08-06 18:40:43 +0900

    Update h-index.py

--- Sorting/h-index.py
+++ Sorting/h-index.py
@@ -1,4 +1,3 @@
-citations = [3, 0, 6, 1, 5, 7, 8, 2, 9]
 
 def solution(citations):
     answer = 0
@@ -6,6 +5,7 @@ def solution(citations):
     #[0, 1, 2, 3, 5, 6, 7, 8, 9]
     #[9, 8, 7, 6, 5, 3, 2, 1, 0]
     #index+1(인용된 논문 갯수)> citation[index](인용횟수)인 최소의 index 찾기?
+    
     index = 0
     citations.sort(reverse=True)
     for i in range(len(citations)):
@@ -15,6 +15,10 @@ def solution(citations):
         else:
             continue
     answer = index
+
+    #논문수<최소 인용수인 경우
+    if answer ==0:
+        if citations[0] > len(citations):
+            answer = len(citations)
     return answer
 
-print(solution(citations))

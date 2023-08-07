22d2ee56287debaa4f3c60dc6f5cc4cfa64f2c2e
seungalee <renazzang01@naver.com>
2023-08-07 23:56:49 +0900

    Update 가장큰수.py
    구글 참조해 푼 것..나중에 다시 풀어보기

--- Sorting/가장큰수.py
+++ Sorting/가장큰수.py
@@ -1,5 +1,12 @@
 def solution(numbers):
     answer = ''
+    strs = []
     for number in numbers:
-        number = 0
+        number = str(number)
+        strs.append(number)
+    #str으로 변환 후 자릿수를 맞춰 비교해 주기 위해 *3
+    strs.sort(key=lambda num: num*3, reverse=True)
+    #int로 변환해주는 이유: 000을 방지하기 위해
+    answer = str(int(''.join(strs)))
+
     return answer

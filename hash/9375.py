9f1462b1230bc4a03bf8d01626bcf210badb9475
seungalee <renazzang01@naver.com>
2023-08-14 10:05:42 +0900

    9375.py 성공
    수학식 인터넷참고

--- hash/9375.py
+++ hash/9375.py
@@ -1,9 +1,9 @@
 import sys
-
 testCaseNum = int(sys.stdin.readline())
-dic = {}
 
 for i in range(testCaseNum):
+    dic = {}
+    num=1
     ClothesNum = int(sys.stdin.readline())
     for j in range(ClothesNum):
         name, type = sys.stdin.readline().rstrip().split()
@@ -12,4 +12,8 @@ for i in range(testCaseNum):
         else:
             dic[type] = [name]
     #총 수 + nC2*a*b+nC3*a*b*c+...+a*b*c*d....
-    
+    ###(a 종류수 +1(착용X 경우))*(b 종류수+1)... -1(모두 안착용하는 경우)
+    for cloth, clothArr in dic.items():
+        num*=(len(clothArr)+1)
+    num-=1
+    print(num)

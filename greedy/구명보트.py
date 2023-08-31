019afa7400137e58ffb5a6fb4663b41595308eb7
seungalee <renazzang01@naver.com>
2023-08-31 22:39:58 +0900

    구명보트 코드 바꿨는데 여전히 효율성은 실패

--- greedy/구명보트.py
+++ greedy/구명보트.py
@@ -1,20 +1,24 @@
 def solution(people, limit):
     answer = 0
     people.sort()
+    i=0
+    j = len(people)-1
     while people:
-        i=0
-        tog_person = 0
         now_person = people[i]
-        people.remove(now_person)
-        pp_len = len(people)
-        for j in range(pp_len):
-            if now_person+people[pp_len - j-1]<=limit:
-                tog_person = people[pp_len -j-1]
-                people.remove(tog_person)
-                answer+=1
-                break
-        if tog_person ==0:
+        if j==0:
+            people.remove(now_person)
             answer+=1
-        i+=1
+            j = len(people)-1
+        elif now_person+people[j]<=limit:
+            people.remove(people[j])
+            people.remove(now_person)
+            answer+=1
+            j = len(people)-1
+        else:
+            j-=1
+        
+        
     return answer
+
+print(solution([70,50,80, 50],100))
 

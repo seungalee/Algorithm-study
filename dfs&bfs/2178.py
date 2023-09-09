4e5ffea43c9a0804934052a404952190682d2d82
seungalee <renazzang01@naver.com>
2023-09-09 17:30:50 +0900

    2178 효율성 증가
    result 배열 삭제

--- dfs&bfs/2178.py
+++ dfs&bfs/2178.py
@@ -11,7 +11,6 @@ for _ in range(N):
     graph.append(list(map(int, arr)))
 
 visit = []
-result = []
 visit.append((0,0))
 graph[0][0] = 1
 
@@ -19,19 +18,14 @@ dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 while visit:
     node = visit.pop(0)
-    if node not in result:
-        result.append(node)
-        canmove = False
-        for i in range(4):
-            newa = node[0]+dx[i]
-            newb = node[1]+dy[i]
-            if 0<=newa<N and 0<=newb<M:
-                if graph[newa][newb]==1:
-                    visit.append((newa,newb))
-                    graph[newa][newb] = graph[node[0]][node[1]]+1
-                    canmove = True
-        if canmove == False: 
-            result.remove(node)
+    for i in range(4):
+        newa = node[0]+dx[i]
+        newb = node[1]+dy[i]
+        if 0<=newa<N and 0<=newb<M:
+            if graph[newa][newb]==1:
+                visit.append((newa,newb))
+                graph[newa][newb] = graph[node[0]][node[1]]+1
+                canmove = True
             
 
 print(graph[N-1][M-1])

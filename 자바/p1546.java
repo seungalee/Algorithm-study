e4883969480fc8b4b49349f9146e53804c8c7691
seungalee <renazzang01@naver.com>
2025-04-03 15:06:54 +0900

    doit 자바 배열
    2문제

---
+++ 자바/p1546.java
@@ -0,0 +1,31 @@
+package 자바;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p1546 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String testScores = sc.nextLine();
+        int[] scoresArr = Arrays.stream(testScores.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int maxScore = 0;
+
+        for(int i=0; i<scoresArr.length; i++){
+            if(scoresArr[i]>maxScore){
+                maxScore = scoresArr[i];
+            }
+        }
+
+        double scoreTotal = 0;
+
+        for(int i=0; i<scoresArr.length;i++){
+            scoreTotal += ((double)scoresArr[i]/maxScore*100);
+        }
+
+        double answer = scoreTotal / scoresArr.length;
+        System.out.print(answer);
+
+    }
+}
